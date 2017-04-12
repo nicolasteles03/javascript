d5d946d4381fc0bba456296d637df614f26b8829
Nícolas Teles <nicolasgt_03@live.com>
2017-04-12 15:35:15 -0300

    Add files via upload

---
+++ exemplo14_1.js
@@ -0,0 +1,11 @@
+function evento_entrar(){
+ alert('seja bem vindo');
+}
+
+function evento_sair(){
+ alert('até a próxima visita');
+}
+
+function evento_clique(){
+ document.getElementById('id2').style.color='red';
+}
